fd71d6c1bccd572543187b0436033acedd066378
envestcc <chen1233216@hotmail.com>
2023-06-28 20:43:29 +0800

    follow sdk sepc

--- client.py
+++ client.py
@@ -1,34 +1,33 @@
-import json
 import requests
+import aiohttp
 import datetime
+from typeguard import typechecked
 
+@typechecked
+class Header:
+    def __init__(self, event_type: str, token: bytes, device_id: str, timestamp: datetime.datetime):
+        self.event_type = event_type
+        self.token = token
+        self.device_id = device_id
+        self.timestamp = timestamp
 
+@typechecked
 class Client:
-    def __init__(self, endpoint, api_key):
+    def __init__(self, endpoint: str, api_key: str):
         self.endpoint = endpoint
         self.api_key = api_key
 
-    # TODO: support push_data non-blocking, which can send data async in batch
-    # TODO: make push_data non-panic
-    def push_data(self, deviceID, data, event_type="DEFAULT"):
-        self._validate_data_format(deviceID, data, event_type)
+    # TODO: support publish_event non-blocking, which can send data async in batch
+    # TODO: make publish_event non-panic
+    def publish_event(self, header: Header, payload: bytes):
         headers = {
-            'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + self.api_key,
         }
         body = [{
-            'device_id': deviceID,
-            'event_type': event_type,
-            'payload': data,
-            'timestamp':  int(round(datetime.datetime.now().timestamp())),
+            'device_id': header.device_id,
+            'event_type': header.event_type,
+            'payload': payload.decode('utf-8'),
+            'timestamp':  int(round(header.timestamp.timestamp())),
         }]
         return requests.post(
-            self.endpoint, data=json.dumps(body), headers=headers)
-
-    def _validate_data_format(self, deviceID, data, event_type):
-        if type(deviceID) != str:
-            raise ValueError("deviceID should be a string")
-        if type(data) != str:
-            raise ValueError("data should be a string")
-        if type(event_type) != str:
-            raise ValueError("event_type should be a string")
+            self.endpoint, json=body, headers=headers)
